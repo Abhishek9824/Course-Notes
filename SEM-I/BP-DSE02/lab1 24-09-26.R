a81d217dc9c5d6586903c5b9175921007b12008c
Abhishek B. <92390777+Abhishek9824@users.noreply.github.com>
2026-09-24 11:22:04 +0530

    Add files via upload

---
+++ SEM-I/BP-DSE02/lab1 24-09-26.R
@@ -0,0 +1,39 @@
+#r <- 5.6
+#print(class(r))
+
+#y <- 8
+#print(class(y))
+
+#x<- as.integer(5)
+#print(class(x))
+
+#x<-3
+#y<-5
+#z<- x>y
+#print(z)
+#print(class(z))
+
+#x <- 4+3i
+#print(class(x))
+
+#x<- "a"
+#print(x)
+#print(class(x))
+
+
+#Vector (1D)
+y<-c(1,2,3,4,5,6,7,8,9,10)
+print(y)
+
+#List examples
+my_list <-list(name = "Apple", age = 30, score= c(90,85,88))
+
+print(my_list)
+
+#Matrix
+m <- matrix(1:6, nrow = 2 , ncol = 3)
+#excute using run then go console then type "m" you will get the matrix
+
+
+
+
