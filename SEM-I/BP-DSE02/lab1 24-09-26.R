e483d5fc72bf00198869b6aabb51cbde5beb4531
Abhishek B. <92390777+Abhishek9824@users.noreply.github.com>
2026-09-25 00:10:11 +0530

    Last updated 24/09/26

--- SEM-I/BP-DSE02/lab1 24-09-26.R
+++ SEM-I/BP-DSE02/lab1 24-09-26.R
@@ -1,24 +1,31 @@
-#r <- 5.6
-#print(class(r))
+#lecture-01 Date-24/09/26
 
-#y <- 8
-#print(class(y))
+#Numeric Data type
+r <- 5.6
+print(class(r))
 
-#x<- as.integer(5)
-#print(class(x))
+y <- 8
+print(class(y))
 
-#x<-3
-#y<-5
-#z<- x>y
-#print(z)
-#print(class(z))
+#Integer Data type
+x<- as.integer(5)
+print(class(x))
 
-#x <- 4+3i
-#print(class(x))
+#logical Data type
+x<-3
+y<-5
+z<- x>y
+print(z)
+print(class(z))
 
-#x<- "a"
-#print(x)
-#print(class(x))
+#Complex Data type
+x <- 4+3i
+print(class(x))
+
+#Character Data type
+x<- "a"
+print(x)
+print(class(x))
 
 
 #Vector (1D)
@@ -33,7 +40,8 @@ print(my_list)
 #Matrix
 m <- matrix(1:6, nrow = 2 , ncol = 3)
 #excute using run then go console then type "m" you will get the matrix
-
+marks <- array(c(80,75,90,70,85,88,23,78,92,76,89,84), dim = c(2,3,2))
+# the dim means the dimension where 2 = rows, 3 = columns , last 2 = two matrices
 
 
 
